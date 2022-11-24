46d86b1a4ba2c98eecde8a20c7a73f29813d8ec9
Abhishek <gautamabhishek0810@gmail.com>
2022-11-24 17:42:12 +0530

    fix:newsitem css

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,66 +7,66 @@ const NewsItem = (props) => {
   let { title, description, imageUrl, newsUrl, author, date, source } = props;
   return (
     <>
-      <div
-        className="card"
-        style={{
-          margin: 3,
-
-          maxWidth: 500,
-          maxHeight: 600,
-        }}
-      >
-        <img
-          src={imageUrl ? imageUrl : NoImage}
-          alt=""
+      <div className="container">
+        <div
+          className="card"
           style={{
-            width: "450px",
-            height: "300px",
-            objectFit: "contain",
-          }}
-        />
+            margin: 3,
 
-        <div className="card-body">
-          <h5 className="card-title">
-            <ReactReadMoreReadLess
-              charLimit={30}
-              readMoreText={"Read more ▼"}
-              readLessText={"Read less ▲"}
-              readMoreClassName="read-more-less--more"
-              readLessClassName="read-more-less--less"
-            >
-              {title}
-            </ReactReadMoreReadLess>
-            <br />
-            <br />
-            <span className="badge bg-secondary">{source}</span>
-          </h5>
-          <p className="card-text">
-            <ReactReadMoreReadLess
-              charLimit={30}
-              readMoreText={"Read more ▼"}
-              readLessText={"Read less ▲"}
-              readMoreClassName="read-more-less--more"
-              readLessClassName="read-more-less--less"
-            >
-              {description}
-            </ReactReadMoreReadLess>
-          </p>
-          <p className="card-text">
-            <small className="text-muted">
-              <b> By-</b> {author ? author : "Unknown"}
+            maxWidth: 500,
+            maxHeight: 600,
+          }}
+        >
+          <div className="container-img">
+            <img
+              className="card-img"
+              src={imageUrl ? imageUrl : NoImage}
+              alt=""
+            />
+          </div>
+
+          <div className="card-body">
+            <h5 className="card-title">
+              <ReactReadMoreReadLess
+                charLimit={30}
+                readMoreText={"Read more ▼"}
+                readLessText={"Read less ▲"}
+                readMoreClassName="read-more-less--more"
+                readLessClassName="read-more-less--less"
+              >
+                {title}
+              </ReactReadMoreReadLess>
               <br />
-              <b>Last updated- </b> {new Date(date).toLocaleString()}
-            </small>
-          </p>
-          <a
-            rel="noreferrer"
-            href={newsUrl}
-            target="_blank"
-            className="btn btn-sm btn-primary"
-          >
-            More Information Click Here
-          </a>
+              <br />
+              <span className="badge bg-secondary">{source}</span>
+            </h5>
+            <p className="card-text">
+              <ReactReadMoreReadLess
+                charLimit={30}
+                readMoreText={"Read more ▼"}
+                readLessText={"Read less ▲"}
+                readMoreClassName="read-more-less--more"
+                readLessClassName="read-more-less--less"
+              >
+                {description}
+              </ReactReadMoreReadLess>
+            </p>
+            <p className="card-text">
+              <small className="text-muted">
+                <b> By-</b> {author ? author : "Unknown"}
+                <br />
+                <b>Last updated- </b> {new Date(date).toLocaleString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-primary"
+            >
+              More Information Click Here
+            </a>
+          </div>
         </div>
       </div>
     </>
